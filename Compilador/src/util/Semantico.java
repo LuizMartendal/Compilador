5f9adbd3d23f9dd647a7ce5a7a9923b85fe63115
KrugerDaniel <danielk872@gmail.com>
2023-11-27 21:51:14 -0300

    continuacao da jornada

--- Compilador/src/util/Semantico.java
+++ Compilador/src/util/Semantico.java
@@ -1,13 +1,16 @@
 package util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
 
 public class Semantico implements Constants {
 
     private HashMap<String, Simbolo> tabela_simbolos = new HashMap<>();
+    private ArrayList<Token> list_id = new ArrayList<>(); 
 
     private Stack<String> pilha_tipos = new Stack<>();
+    private String operadorRelacional;
     private static String codigo = "";
 
     public void executeAction(int action, Token token)	throws SemanticError
@@ -21,7 +24,7 @@ public class Semantico implements Constants {
             case 101:
                 acao101();
                 break;
-            case 103:
+            case 131:
                 acao131(token);
                 break;
             case 114:
@@ -38,6 +41,51 @@ public class Semantico implements Constants {
                 break;
             case 116:
                 acao116(token);
+                break;
+            case 117:
+                acao117();
+                break;
+            case 110:
+                acao110();
+                break;
+            case 111:
+                acao111();
+                break;
+            case 112:
+                acao112();
+                break;
+            case 113:
+                acao113();
+                break;
+            case 108:
+                acao108(token);
+                break;
+            case 109:
+                acao109();
+                break;
+            case 107:
+                acao107();
+                break;
+            case 103:
+                acao103();
+                break;
+            case 104:
+                acao104();
+                break;
+            case 102:
+                acao102();
+                break;
+            case 125:
+                acao125(token);
+                break;
+            case 126:
+                acao126(token);
+                break;
+            case 127:
+                acao127(token);
+                break;
+            case 128:
+                acao128(token);
                 break;
         }
     }
@@ -130,5 +178,164 @@ public class Semantico implements Constants {
 
     public void acao116(Token token) {
         adicionaString(token.getLexeme());
+    }
+
+    public void acao117() {
+        codigo += "ldc.i8 -1\n";
+        codigo += "conv.r8\n";
+        codigo += "mul\n";
+    }
+
+    public void acao110() {
+        tabelaTipos();
+        codigo += "add\n";
+    }
+
+    public void acao111() {
+        tabelaTipos();
+        codigo += "sub\n";
+    }
+
+    public void acao112() {
+        tabelaTipos();
+        codigo += "mul\n";
+    }
+
+    public void acao113() {
+        tabelaTipos();
+        codigo += "div\n";
+    }
+
+    public void tabelaTipos() {
+        String operador2 = pilha_tipos.pop();
+        String operador1 = pilha_tipos.pop();
+
+
+        if (operador1.equals("float64") || operador2.equals("float64")) {
+            pilha_tipos.push("float64");
+            return;
+        }
+        pilha_tipos.push("int64");
+        return;
+    }
+
+    public void acao108(Token token) {
+        operadorRelacional = "" + token.getLexeme();
+    }
+
+    public void acao109() {
+        pilha_tipos.pop();
+        pilha_tipos.pop();
+        pilha_tipos.push("bool");
+        this.operadorRelacional();
+    }
+
+    public void operadorRelacional() {
+        switch (operadorRelacional) {
+            case "==":
+                codigo += "ceq\n";
+                break;
+            case "!=":
+                codigo += "ceq\n" + "ldc.i4.0\n" + "ceq\n";
+                break;
+            case ">":
+                codigo += "cgt\n";
+                break;
+            case "<":
+                codigo += "clt\n";
+                break;
+        }
+    }
+
+    public void acao107() {
+        codigo += "ldc.i4.1\nxor\n";
+    }
+
+    public void acao103() {
+        pilha_tipos.pop();
+        pilha_tipos.pop();
+        pilha_tipos.push("bool");
+        codigo += "and\n";
+    }
+
+    public void acao104() {
+        pilha_tipos.pop();
+        pilha_tipos.pop();
+        pilha_tipos.push("bool");
+        codigo += "or\n";
+    }
+
+    public void acao102() {
+        String tipo = pilha_tipos.pop();
+        if (tipo.equals("int64")) {
+            codigo += "conv.i8\n";
+        }
+        this.print(tipo);
+    }
+
+    public void print(String tipo) {
+        codigo += "call void [mscorlib]System.Console::WriteLine(" + tipo + ")\n";
+    }
+
+    public void acao125(Token token) {
+        list_id.add(token);
+    }
+
+    public void acao126(Token token) throws SemanticError {
+        for (Token t : this.list_id) {
+            if (tabela_simbolos.containsKey(t.getLexeme())) {
+                throw new SemanticError(t.getLexeme() + " já declarado", t.getPosition());
+            }
+            Simbolo simbolo = this.createSimbolo(t, token);
+            tabela_simbolos.put(t.getLexeme(), simbolo);
+        }
+        this.list_id.clear();
+    }
+
+    public Simbolo createSimbolo(Token token1, Token token2) {
+        Simbolo simbolo = new Simbolo();
+        if (token1.getLexeme().startsWith("_i")) {
+            simbolo.setTipo("int64");
+        } else if (token1.getLexeme().startsWith("_f")) {
+            simbolo.setTipo("float64");
+        } else if (token1.getLexeme().startsWith("_s")) {
+            simbolo.setTipo("string");
+        } else {
+            simbolo.setTipo("bool");
+        }
+        simbolo.setValor(token2.getLexeme());
+        simbolo.setIdentifidor(token1.getLexeme());
+        simbolo.setConstante(true);
+        return simbolo;
+    }
+
+    public void acao127(Token token) throws SemanticError {
+        for (Token t : this.list_id) {
+            if (tabela_simbolos.containsKey(t.getLexeme())) {
+                throw new SemanticError(t.getLexeme() + " já declarado", t.getPosition());
+            }
+            Simbolo simbolo = this.createSimbolo(t, token);
+            tabela_simbolos.put(t.getLexeme(), simbolo);
+            codigo += ".locals " + simbolo.getTipo() + " " + simbolo.getIdentifidor() + "\n";
+        }
+        this.list_id.clear();
+    }
+
+    public void acao128(Token token) throws SemanticError {
+        String tipo = this.pilha_tipos.pop();
+        for (int i = 0; i < this.list_id.size(); i++) {
+            codigo += "dup\n";
+        }
+
+        for (Token t : this.list_id) {
+            if (!tabela_simbolos.containsKey(t.getLexeme())) {
+                throw new SemanticError(t.getLexeme() + " não declarado", t.getPosition());
+            }
+            if (tipo.equals("int64")) {
+                codigo += "conv.i8\n";
+            }
+            codigo += "stloc " + t.getLexeme() + "\n";
+        }
+        this.list_id.clear();
     }
 }
